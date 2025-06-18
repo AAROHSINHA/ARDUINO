6527388b30e804c81fcc71fb35e982cab7fe17f5
AAROH_SINHA__ <167206572+AAROHSINHA@users.noreply.github.com>
2025-06-19 03:14:11 +0530

    Create 04_CONTINUOUS_LED_BLINKING.ino

---
+++ DOCS/04_CONTINUOUS_LED_BLINKING.ino
@@ -0,0 +1,138 @@
+void setup() {
+  // for red led - pin 8 is used
+  pinMode(8, OUTPUT); // parameters - [what pin we are working with, whether that pin is input or output]
+  // for green led - pin 9 is used
+  pinMode(9, OUTPUT);
+  // for blue led - pin 10 is used
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // for red - 5 times blink
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  
+  // for green - 10 times blink
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+
+  // for blue - 15 times blink
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+
+
+}
